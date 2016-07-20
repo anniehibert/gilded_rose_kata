984e94a3937d6186615db9fd949c8a9c976174d9
anniehibert <ahibertjr@gmail.com>
2016-07-20 18:45:15 -0500

    added brie cheese

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -6,48 +6,27 @@ class GildedRose
   end
 
   def tick
-    if @name != 'Aged Brie' && @name != 'Backstage passes to a TAFKAL80ETC concert'
-      if @quality > 0
-        if @name != 'Sulfuras, Hand of Ragnaros'
-          @quality -= 1
-        end
+    if @name == 'normal' && @quality > 0
+      if @days_remaining > 0
+        @quality -= 1
+      else
+        @quality -= 2
       end
-    else
-      if @quality < 50
-        @quality += 1
-        if @name == 'Backstage passes to a TAFKAL80ETC concert'
-          if @days_remaining < 11
-            if @quality < 50
-              @quality += 1
-            end
-          end
-          if @days_remaining < 6
-            if @quality < 50
-              @quality += 1
-            end
-          end
-        end
-      end
-    end
-    if @name != 'Sulfuras, Hand of Ragnaros'
+    @days_remaining -= 1
+    elsif @quality == 0
       @days_remaining -= 1
     end
-    if @days_remaining < 0
-      if @name != "Aged Brie"
-        if @name != 'Backstage passes to a TAFKAL80ETC concert'
-          if @quality > 0
-            if @name != 'Sulfuras, Hand of Ragnaros'
-              @quality -= 1
-            end
-          end
-        else
-          @quality = @quality - @quality
-        end
-      else
-        if @quality < 50
-          @quality += 1
-        end
+  
+    if @name == 'Aged Brie' && @quality < 50
+      @days_remaining -= 1
+      if quality == 49 || @days_remaining > 0
+        @quality += 1
+      elsif @days_remaining <= 0
+        @quality += 2
       end
+    elsif @quality >= 50
+      @days_remaining -= 1
     end
+
   end
 end
